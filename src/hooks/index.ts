2f48f9725726f6cbffa7c6b3db7710058b51d5e5
yoon12345678910 <yji1221@gmail.com>
2024-01-03 11:49:29 +0900

    feat: usePrevious 추가 및 index 수정

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
-export * from './useFetchMockData';
-export * from './useIntersectionObserver';
+export { default as useFetchMockData } from './useFetchMockData';
+export { default as useIntersectionObserver } from './useIntersectionObserver';
+export { default as usePrevious } from './usePrevious';
